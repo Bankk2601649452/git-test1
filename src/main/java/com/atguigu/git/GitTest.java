1b05e3166b5436b7107960dfa7b67b7c8304e613
Bankk <2601649452@qq.com>
2021-05-16 10:58:52 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git1234");
         System.out.println("master test");
         System.out.println("hot-fix test1");
+        System.out.println("push test ");
     }
 }
